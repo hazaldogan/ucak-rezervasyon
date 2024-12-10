f2ee72c32fe5c28004ba0507f27562559a250c16
Hazal Taştan <hazl.dogann@gmail.com>
2024-12-11 01:20:57 +0300

    spring boot kurulumu yapıldı

---
+++ src/main/java/com/teamso/ucak_rezervasyon/UcakRezervasyonApplication.java
@@ -0,0 +1,13 @@
+package com.teamso.ucak_rezervasyon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UcakRezervasyonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UcakRezervasyonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/teamso/ucak_rezervasyon/UcakRezervasyonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamso.ucak_rezervasyon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UcakRezervasyonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
